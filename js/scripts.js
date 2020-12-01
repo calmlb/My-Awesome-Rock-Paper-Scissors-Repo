40bd8ceef644c8195c566971ea9e56c2b8ac2a0e
eric mathias <egm@erics-mbp.lan>
2020-12-01 16:45:53 -0500

    win logic done

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-console.log("")
-// Constants
+/* ----- constants ------- */
 
 const options = ['R', 'P', 'S']
 const winningCombos = ['RS', 'PR', 'SP']
 
-// apps state (variables) for state
+/* ------ apps state (variables) for state ------- */
 
 let scores = {
     player: 0,
@@ -33,10 +32,55 @@ let computerImage = document.getElementById('computer-output')
 let playButton = document.getElementById('play')
 let restButton = document.getElementById('reset')
 
-getElementsByClassName /*never use - always querySelector and querySelectorAll and getElementById */
-getElementsById /*always fastest because unique, so fast to search*/
-querySelectorAllquerySelectorAll
-// event listeners
+let choices = document.getElementById('choices')
 
+// Event Listeners
+playButton.addEventListener('click', playGame)
+resetButton.addEventListener('click', init)
+choices.addEventListener('click', choiceSelect())
 
-// functions
+// Functions
+function init() {
+    scores = {
+        player: 0,
+        comp: 0,
+        ties: 0
+    }
+    render();
+
+}
+
+function playGame() {
+    choices.classList.remove('hidden')
+    playButton.classList.add('hidden')
+}
+
+function choiceSelect(event) {
+    playerChoice = event.target.id)
+    compSelect()
+    checkWinner()
+    render()
+}
+    // handle comp choice
+    //declare winner
+    //give out points
+    //play button resumes unhidden
+    render()
+}
+
+function compSelect () {
+compChoice = options[Math.floor(Math.random() * 3)] 
+}
+
+function checkWinner () {
+    // Tie condition
+    if (playerChoice === compChoice) {
+    scores.ties++
+    // Player Wins
+    } else if (winningCombos.includes(playerChoice + compChoice)) {
+        scores.player++
+    // Computer Wins
+    } else {
+        scores.comp++
+    }
+}
